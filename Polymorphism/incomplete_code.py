5437813322b64096ac4bdceb5fc7a1c1a703ef00
titichayajojo <titichaya_love@hotmail.com>
2021-02-18 17:20:57 +0700

    Update incomplete_code.py
    fix bugs

--- Polymorphism/incomplete_code.py
+++ Polymorphism/incomplete_code.py
@@ -21,17 +21,35 @@ class Walk( Transportation ):
    def find_cost( self ):
       return 0
 
+   
+class Taxi( Transportation ):
+
+   def __init__( self, start, end, distance ):
+      Transportation.__init__( self, start, end, distance)
+
+   def find_cost( self ):
+      return 40 * self.distance
+
+class Train( Transportation ):
+
+   def __init__( self, start, end, distance,station ):
+      Transportation.__init__( self, start, end, distance)
+      self.station = station
+
+   def find_cost( self ):
+      return 5 * self.station
+
 
    
 # main program
 
 travel_cost = 0
 
-#trip = [ Walk("KMITL","KMITL SCB Bank",0.6),
-         #Taxi("KMITL SCB Bank","Ladkrabang Station",5),
-         #Train("Ladkrabang Station","Payathai Station",40,6),
-         #Taxi("Payathai Station","The British Council",3) ]
+trip = [ Walk("KMITL","KMITL SCB Bank",0.6),
+         Taxi("KMITL SCB Bank","Ladkrabang Station",5),
+         Train("Ladkrabang Station","Payathai Station",40,6),
+         Taxi("Payathai Station","The British Council",3) ]
 
 for travel in trip:
    travel_cost += travel.find_cost()
-print travel_cost
+print(travel_cost)
